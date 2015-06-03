838bedc9b86f9e89d2a7039015f1c316d2db169f
zhhoper <zhhoper2010@gmail.com>
2015-06-03 16:09:08 -0400

    changes for synthesize_data

--- syntheszie_data.m
+++ syntheszie_data.m
@@ -1,4 +1,4 @@
-function [label, data, inter_s, intra_s] = syntheszie_data(numClass, numSamples, dim)
+function [label, data, inter_s, intra_s] = syntheszie_data(numClass, numSamples, dim, s_inter, s_intra)
 % [label, data] = syntheszie_data(numClass, numSamples, dim)
 % this function is used to generate data and label
 % numClass : number of classes
@@ -7,7 +7,7 @@ function [label, data, inter_s, intra_s] = syntheszie_data(numClass, numSamples,
 
 %% generate inter class variance
 rng(5000);
-eigValue1 = rand(dim,1) * 2 ;
+eigValue1 = rand(dim,1) * s_inter ;
 vec1 = zeros(dim, dim);
 for i = 1 : dim
     rng(5000 + i*20);
@@ -15,6 +15,7 @@ for i = 1 : dim
     if i == 1
         vec1(:,i) = tmp / norm(tmp);
     else
+        % orthogonalize the generated data
         tmpVec = vec1(:, 1:i-1);
         tmp = tmp - tmpVec*(tmpVec'*tmp);
         tmp = tmp / norm(tmp);
@@ -25,7 +26,7 @@ inter_s = vec1*diag(eigValue1)*vec1';
 
 %% generate intra class variance
 rng(10000);
-eigValue2 = rand(dim,1)*5;
+eigValue2 = rand(dim,1) * s_intra;
 vec2 = zeros(dim, dim);
 for i = 1 : dim
     rng(10000 + i*20);
@@ -33,6 +34,7 @@ for i = 1 : dim
     if i == 1
         vec1(:,i) = tmp / norm(tmp);
     else
+        % orthogonalize the generated data
         tmpVec = vec2(:, 1:i-1);
         tmp = tmp - tmpVec*(tmpVec'*tmp);
         tmp = tmp / norm(tmp);
